33a09c3e6063a0a0091a6bfb4e9933c7152fa272
Yann Courtel <courtel.yann@gmail.com>
2024-05-16 21:32:50 +0200

    week 5 exercise 1 solution

--- solution/java/week05/exercise1/src/main/java/games/FizzBuzz.java
+++ solution/java/week05/exercise1/src/main/java/games/FizzBuzz.java
@@ -1,36 +1,36 @@
 package games;
 
-import java.security.Provider;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FizzBuzz {
-    public static final int MIN = 0;
-    public static final int MAX = 100;
-    public static final int FIZZ = 3;
-    public static final int BUZZ = 5;
-    public static final int FIZZBUZZ = 15;
+    private static final int MIN = 1;
+    private static final int MAX = 100;
+    private static final int FIZZ = 3;
+    private static final int BUZZ = 5;
+    private static final int FIZZBUZZ = 15;
+    private static final boolean DEFAULT = true;
 
-    private FizzBuzz() {
+    private static final Map<Predicate<Integer>, Function<Integer, String>> mapping;
+
+    static {
+        mapping = new LinkedHashMap<>();
+        mapping.put(i -> is(FIZZBUZZ, i), i -> "FizzBuzz");
+        mapping.put(i -> is(FIZZ, i), i -> "Fizz");
+        mapping.put(i -> is(BUZZ, i), i -> "Buzz");
+        mapping.put(i -> DEFAULT, Object::toString);
     }
 
     public static String convert(Integer input) throws OutOfRangeException {
-        if (isOutOfRange(input)) {
-            throw new OutOfRangeException();
-        }
-        return convertSafely(input);
-    }
-
-    private static String convertSafely(Integer input) {
-        if (is(FIZZBUZZ, input)) {
-            return "FizzBuzz";
-        }
-        if (is(FIZZ, input)) {
-            return "Fizz";
-        }
-        if (is(BUZZ, input)) {
-            return "Buzz";
-        }
-        return input.toString();
+        return mapping.entrySet()
+                .stream()
+                .filter(f -> !isOutOfRange(input))
+                .filter(f -> f.getKey().test(input))
+                .findFirst()
+                .map(v -> v.getValue().apply(input))
+                .orElseThrow(OutOfRangeException::new);
     }
 
     private static boolean is(Integer divisor, Integer input) {
@@ -38,6 +38,6 @@ public class FizzBuzz {
     }
 
     private static boolean isOutOfRange(Integer input) {
-        return input <= MIN || input > MAX;
+        return input < MIN || input > MAX;
     }
 }
