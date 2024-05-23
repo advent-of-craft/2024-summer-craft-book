2597aa2450e57fd4a95060241d45963b0835a9a7
Yann Courtel <courtel.yann@gmail.com>
2024-05-23 12:10:33 +0200

    solution week 3 exercise 2

--- solution/java/week03/exercise2/src/main/java/cache/Cache.java
+++ solution/java/week03/exercise2/src/main/java/cache/Cache.java
@@ -4,12 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Cache {
-    private Map<String, Integer> map = new HashMap<String, Integer>();
+    private final Map<String, Integer> map = new HashMap<>();
 
-    public int getOrInsert(String key, int value) {
-        if (!map.containsKey(key)) {
-            map.put(key, value);
-        }
-        return map.get(key); // Modifies state if key not present and returns value
+    public Integer get(String key) {
+        return map.get(key); // Just returns the value or null if not present
+    }
+
+    public void insertIfAbsent(String key, int value) {
+        map.putIfAbsent(key, value); // Only modifies the state, does not return value
+    }
+
+    public int ensurePresentAndGet(String key, int defaultValue) {
+        insertIfAbsent(key, defaultValue);
+        return get(key);
     }
 }
