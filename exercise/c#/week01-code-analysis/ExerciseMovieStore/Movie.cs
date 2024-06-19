33034b486ca41e1ad2d86567d6f2569b8e5f7aa4
Yann Courtel <courtel.yann@gmail.com>
2024-06-19 22:30:58 +0200

    adding week1 in C#

---
+++ exercise/c#/week01-code-analysis/ExerciseMovieStore/Movie.cs
@@ -0,0 +1,27 @@
+﻿namespace ExerciseMovieStore
+{
+    public class Movie
+    {
+        public string MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public double UnitPrice { get; set; }
+        public int TotalCopies { get; set; }
+        public int BorrowedCopies { get; set; }
+
+        public Movie(string movieID, string title, string director, int totalCopies, double unitPrice)
+        {
+            MovieID = movieID;
+            Title = title;
+            Director = director;
+            TotalCopies = totalCopies;
+            UnitPrice = unitPrice;
+            BorrowedCopies = 0;
+        }
+
+        public bool CanSell()
+        {
+            return UnitPrice != 0d;
+        }
+    }
+}
