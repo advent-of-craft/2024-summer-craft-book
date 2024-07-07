a44e99fe2308e78dd6a02755cebd7bded74d77d1
Yoan Thirion <yoan.thirion@gmail.com>
2024-07-07 16:24:52 +0200

    Upgrade packages

--- solution/c#/week06-legacy-code/ReportGenerator.Tests/ReportGeneratorTests.cs
+++ solution/c#/week06-legacy-code/ReportGenerator.Tests/ReportGeneratorTests.cs
@@ -1,52 +1,47 @@
 using FluentAssertions;
+using FluentAssertions.LanguageExt;
 using LanguageExt;
-using Xunit;
+using LanguageExt.UnsafeValueAccess;
 
 namespace ReportGenerator.Tests
 {
     public class ReportGeneratorTests
     {
         [Fact]
-        public void Generate_Report_In_Csv() =>
-            CreateReportGenerator()
-                .GenerateReport(ReportType.Csv, CreateReportData())
-                .Match(
-                    Right: report =>
-                        report.Should()
-                            .Be(
-                                "Starting CSV Report Generation...\nCSV Header: ID, Value, Description\n1,100,Sample Data 1\n2,200,Sample Data 2\nCSV Report Generated Successfully."),
-                    Left: error => Assert.Fail($"Unexpected error: {error}"));
+        public Task Generate_Report_In_Csv() =>
+            Verify(
+                CreateReportGenerator()
+                    .GenerateReport(ReportType.Csv, CreateReportData())
+                    .ValueUnsafe()
+            );
 
         [Fact]
-        public void Generate_Report_In_Pdf() =>
-            CreateReportGenerator()
-                .GenerateReport(ReportType.Pdf, CreateReportData())
-                .Match(
-                    Right: report => report.Should().Be(
-                        "Starting PDF Report Generation...\nPDF Report Title: Comprehensive Data Report\n------------------------------------------------\nData ID: 1 | Data Value: 100 | Description: Sample Data 1\nData ID: 2 | Data Value: 200 | Description: Sample Data 2\n------------------------------------------------\nPDF Report Generated Successfully."),
-                    Left: error => Assert.Fail($"Unexpected error: {error}")
-                );
+        public Task Generate_Report_In_Pdf() =>
+            Verify(
+                CreateReportGenerator()
+                    .GenerateReport(ReportType.Pdf, CreateReportData())
+                    .ValueUnsafe()
+            );
 
         [Fact]
         public void Generate_Report_Should_Fail_If_Report_Data_Is_Empty() =>
             CreateReportGenerator()
-                .GenerateReport(ReportType.Pdf, []).Match(
-                    Right: report => Assert.Fail("Expected an error but got a report"),
-                    Left: error => error
-                        .Should()
-                        .Be(new NoDataToReport("No data provided for report generation."))
-                );
+                .GenerateReport(ReportType.Pdf, [])
+                .Should()
+                .BeLeft(error => error
+                    .Should()
+                    .Be(new NoDataToReport("No data provided for report generation.")
+                    ));
 
         [Fact]
         public void Generate_Report_Should_Fail_If_Report_Type_Not_Supported() =>
             CreateReportGenerator()
                 .GenerateReport(ReportType.Unsupported, CreateReportData())
-                .Match(
-                    Right: report => Assert.Fail("Expected an error but got a report"),
-                    Left: error => error
-                        .Should()
-                        .Be(new UnsupportedReportType("Report type Unsupported not supported."))
-                );
+                .Should()
+                .BeLeft(error => error
+                    .Should()
+                    .Be(new UnsupportedReportType("Report type Unsupported not supported.")
+                    ));
 
         private static ReportGenerator CreateReportGenerator() =>
             new(new Dictionary<ReportType, ICanGenerateReport>
